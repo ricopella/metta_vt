abee3dd08fa8ae3d7a3fdd77128ae94e7335f4dd
Narin Sundarabhaya <nrs710@gmail.com>
2020-06-13 17:49:57 -0700

    ✨ add theme provider

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,9 +1,13 @@
+import createPersistedState from 'use-persisted-state'
 import Footer from '../Footer'
 import NORMALIZE from '../../styles/normalize'
 import React, { FC } from 'react'
 import SEO from '../SEO'
 import styled from '../../styled'
+import ThemeContext from '../../providers/ThemeContext'
+import { DARK_MODE_THEME, LIGHT_MODE_THEME } from '../../styles/variables'
 import { Global } from '@emotion/core'
+import { ThemeProvider } from 'emotion-theming'
 
 interface ILayout {
   title?: string
@@ -15,15 +19,22 @@ const PageWrapper = styled.div`
   margin: 0 auto;
 `
 
+const useThemeState = createPersistedState("theme")
 const Layout: FC<ILayout> = ({ children, title }) => {
+  const [isThemeDark, setIsThemeDark] = useThemeState(false)
+
   return (
     <>
       <Global styles={NORMALIZE} />
       <SEO title={title} />
-      <PageWrapper>
-        <Main>{children}</Main>
-        <Footer />
-      </PageWrapper>
+      <ThemeContext.Provider value={{ isThemeDark, setIsThemeDark }}>
+        <ThemeProvider theme={isThemeDark ? DARK_MODE_THEME : LIGHT_MODE_THEME}>
+          <PageWrapper>
+            <Main>{children}</Main>
+            <Footer />
+          </PageWrapper>
+        </ThemeProvider>
+      </ThemeContext.Provider>
     </>
   )
 }
